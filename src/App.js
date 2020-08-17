5f2f4560e1de946c5d5ee41faf5e7943d2dd2649
Rojin Abraham <rojinabraham@gmail.com>
2020-08-17 18:13:30 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import seedColors from "./seedColors";
+import "./App.css";
+import Palette from "./Palette";
+
+function App() {
+  return (
+    <div className="App">
+      <Palette {...seedColors[4]}></Palette>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Palette.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Palette extends Component {
+  render() {
+    return (
+      <div className="Pallete">
+        <div className="Palette-colors">{/* {color boxes} */}</div>
+        {/* {footer} */}
+      </div>
+    );
+  }
+}
+
+export default Palette;
