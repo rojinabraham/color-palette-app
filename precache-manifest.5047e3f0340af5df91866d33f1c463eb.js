0c8599be8e51e61c3271032813f87d0444d34454
Rojin Abraham <rojinabraham@gmail.com>
2020-08-18 18:42:11 +0530

    Updates

---
+++ precache-manifest.5047e3f0340af5df91866d33f1c463eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b50f65dffe34865e512c465307fe4890",
+    "url": "/color-palette-app/index.html"
+  },
+  {
+    "revision": "213dbca6b0fbdaf2fddc",
+    "url": "/color-palette-app/static/css/2.ed590494.chunk.css"
+  },
+  {
+    "revision": "565753f4c767763aa301",
+    "url": "/color-palette-app/static/css/main.fa6c2177.chunk.css"
+  },
+  {
+    "revision": "213dbca6b0fbdaf2fddc",
+    "url": "/color-palette-app/static/js/2.c1638f70.chunk.js"
+  },
+  {
+    "revision": "fe2f19c5d5965d51615a596cf8d07fe9",
+    "url": "/color-palette-app/static/js/2.c1638f70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "565753f4c767763aa301",
+    "url": "/color-palette-app/static/js/main.eb86ef2e.chunk.js"
+  },
+  {
+    "revision": "57e2c86b757b2fc4dc5d",
+    "url": "/color-palette-app/static/js/runtime-main.3e52b89c.js"
+  }
+]);
