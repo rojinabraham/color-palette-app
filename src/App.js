bb4ceb72ae52c8c2443b7a2af5aded33ad1373b0
Rojin Abraham <rojinabraham@gmail.com>
2020-08-18 13:32:59 +0530

    added slider

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { Component } from "react";
 import seedColors from "./seedColors";
 import "./App.css";
 import Palette from "./Palette";
+import { generatePalette } from "./colorHelpers";
 
-function App() {
-  return (
-    <div>
-      <Palette {...seedColors[4]}></Palette>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Palette palette={generatePalette(seedColors[4])}></Palette>
+      </div>
+    );
+  }
 }
 
 export default App;
